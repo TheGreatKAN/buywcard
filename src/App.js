1fe517de29f10f02b5ab13fe4a1f17cb81fdaa0f
topleft1562 <87454455+topleft1562@users.noreply.github.com>
2022-06-15 10:13:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -355,17 +355,25 @@ function App() {
 
                 </s.TextDescription>
                 <s.SpacerXSmall />
-                <StyledButton
+               /*
+                  <StyledButton
                       onClick={(e) => {
                         <WinterCheckout 
-    projectId={1090} 
-    production={true} 
-    showModal={false} 
-/>
+                            projectId={1090} 
+                            production={true} 
+                            showModal={false} 
+                         />
                       } }
                     >
                       Buy With Card
                     </StyledButton>
+                    */
+                  
+                  <WinterCheckout 
+                    projectId={1090} 
+                    production={true} 
+                    showModal={true} 
+                  />
 
                 
                 {blockchain.account === "" ||
